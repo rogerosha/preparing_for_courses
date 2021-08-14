8c794d82b5c3d3a1456ca31a86f1d93570277b05
Kate Sharova <71977925+rogerosha@users.noreply.github.com>
2021-08-14 17:01:23 +0300

    added a guideline

--- lucky_ticket.js
+++ lucky_ticket.js
@@ -1,4 +1,5 @@
 const ticket = '';
+//write down a six-digit number (for example, 806325)
 const firstHalf = ticket.slice(0, 3); 
 const secondHalf = ticket.slice(3, 6);
 let firstNumber = +firstHalf[0], secondNumber = +firstHalf[1], thirdNumber = +firstHalf[2], fourthNumber = +secondHalf[0], fifthNumber = +secondHalf[1], sixthNumber = +secondHalf[2];
