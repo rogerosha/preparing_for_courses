cb9ed1743f8958e5dba20d8ea75122210c6301ad
Kate Sharova <71977925+rogerosha@users.noreply.github.com>
2021-08-14 16:53:40 +0300

    commited lucky ticket

---
+++ lucky_ticket.js
@@ -0,0 +1,10 @@
+const ticket = '';
+const firstHalf = ticket.slice(0, 3); 
+const secondHalf = ticket.slice(3, 6);
+let firstNumber = +firstHalf[0], secondNumber = +firstHalf[1], thirdNumber = +firstHalf[2], fourthNumber = +secondHalf[0], fifthNumber = +secondHalf[1], sixthNumber = +secondHalf[2];
+let firstSum = firstNumber + secondNumber + thirdNumber; secondSum = fourthNumber + fifthNumber + sixthNumber;
+if (firstSum != secondSum) {
+    console.log('Sorry, your ticket is not lucky. Try next time!')
+} else {
+    console.log('Congratulations! Your ticket is lucky!')
+}
