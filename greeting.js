665a71f8db941557aa31b580070ddd0faa19417c
Kate Sharova <71977925+rogerosha@users.noreply.github.com>
2021-08-19 16:28:37 +0300

    commited func greeting

---
+++ greeting.js
@@ -0,0 +1,10 @@
+function greeting(name) {
+    if (name == 'Johnny') {
+        return 'Hello, my dear';
+    } else {
+        return 'Hello ' + name;
+    };
+};
+
+greet = greeting('Johnny');
+console.log(greet);
