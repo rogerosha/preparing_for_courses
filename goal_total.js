2e35fe3137162ed4ef07f462088dba6d7c4b80eb
Kate Sharova <71977925+rogerosha@users.noreply.github.com>
2021-08-19 16:59:37 +0300

    commited total goals

---
+++ goal_total.js
@@ -0,0 +1,6 @@
+const laLigaGoals = 43;
+const championsLeagueGoals = 10;
+const copaDelReyGoals = 5;
+const totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals;
+
+console.log(totalGoals);
