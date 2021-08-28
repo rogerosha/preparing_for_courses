e49677e185ff7a0a0a52c731189921653154d89f
Kate Sharova <71977925+rogerosha@users.noreply.github.com>
2021-08-28 16:42:01 +0300

    rewrote using while loop

--- removing_exclamation_marks.js
+++ removing_exclamation_marks.js
@@ -1,2 +1,13 @@
-const string = 'hey!';
-console.log(string.slice(0, -1))
+function remove(string) {
+    while (string[string.length - 1] === '!') {
+        string = string.slice(0, -1);
+    }
+    return string;
+}
+
+console.log(remove("Hi!") === "Hi")
+console.log(remove("Hi!!!") === "Hi")
+console.log(remove("!Hi") === "!Hi")
+console.log(remove("!Hi!") === "!Hi")
+console.log(remove("Hi! Hi!") === "Hi! Hi")
+console.log(remove("Hi") === "Hi")
