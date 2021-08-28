5d6df12d9e37cf1fb6bbc74edd4f63c408af357e
Kate Sharova <71977925+rogerosha@users.noreply.github.com>
2021-08-28 16:43:44 +0300

    a little fix

--- removing_exclamation_marks.js
+++ removing_exclamation_marks.js
@@ -5,9 +5,9 @@ function remove(string) {
     return string;
 }
 
-console.log(remove("Hi!") === "Hi")
-console.log(remove("Hi!!!") === "Hi")
-console.log(remove("!Hi") === "!Hi")
-console.log(remove("!Hi!") === "!Hi")
-console.log(remove("Hi! Hi!") === "Hi! Hi")
+console.log(remove("Hi!") === "Hi");
+console.log(remove("Hi!!!") === "Hi");
+console.log(remove("!Hi") === "!Hi");
+console.log(remove("!Hi!") === "!Hi");
+console.log(remove("Hi! Hi!") === "Hi! Hi");
 console.log(remove("Hi") === "Hi")
